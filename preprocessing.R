742fc07c0235720933b2619882f4c5f828f9f75b
neanderthalensis <atyrlik@terpmail.umd.edu>
2024-08-14 13:44:31 -0400

    distance calculation

--- preprocessing.R
+++ preprocessing.R
@@ -22,9 +22,9 @@ dat <- dat %>%
 ElbowPlot(dat)
 
 dat <- dat %>%
-  FindNeighbors(dims = 1:14) %>%
+  FindNeighbors(dims = 1:20) %>%
   FindClusters(resolution = 0.3) %>%
-  RunUMAP(dims = 1:14)
+  RunUMAP(dims = 1:20)
 
 dat <- dat %>%
   NormalizeData(assay = "ATAC") %>%
@@ -39,25 +39,3 @@ dat <- dat %>%
   FindClusters(resolution = 0.3) %>%
   RunUMAP(dims = 1:14, reduction = "pca_ATAC", reduction.name = "umap_ATAC")
 
-norm <- VST(Hackathon2024.RNA)
-
-vars <- apply(Hackathon2024.RNA, 1, var)
-mean <- apply(Hackathon2024.RNA, 1, mean)
-norm <- VST(Hackathon2024.RNA)
-
-df <- data.frame(vars = vars, mean = mean, stvars = norm$variance.standardized)
-
-rna <- Hackathon2024.RNA[rownames(Hackathon2024.RNA) == "YPEL5",]
-atac <- Hackathon2024.ATAC[rownames(Hackathon2024.ATAC) == "chr2-30144160-30169627",]
-
-df <- data.frame(rna = rna, atac = atac)
-ggplot(df, aes(x = rna, y = atac)) + geom_point() +
-  geom_smooth(method = "lm")
-
-train <- Hackathon2024.Training.Set.Peak2Gene.Pairs
-
-apply(Hackathon2024.RNA, 1, function(x){
-  this <- Hackathon2024.Training.Set.Peak2Gene.Pairs[Hackathon2024.Training.Set.Peak2Gene.Pairs$gene == rownames(x),]$peak
-  y <- Hackathon2024.ATAC[rownames(Hackathon2024.ATAC) == this,]
-  # cor(x, y)
-})
--- setgeneration.R
+++ setgeneration.R
@@ -1,6 +1,6 @@
 library(Seurat)
 library(tidyverse)
-
+library(biomaRt)
 
 nrommat <- dat@assays$RNA$data
 atacnormmat <- dat@assays$ATAC$data
@@ -32,7 +32,30 @@ atacsets <- lapply(atacsets, function(x){
 })
 names(atacsets) <- rownames(ataczmat)
 
+ensembl <- useMart("ensembl")
+ensembl <- useDataset("hsapiens_gene_ensembl",mart=ensembl)
+martout <- getBM(attributes=c('hgnc_symbol', 'chromosome_name', 'start_position', 'end_position'),
+      filters= 'hgnc_symbol',
+      values= toupper(Hackathon2024.Training.Set.Peak2Gene.Pairs$gene),
+      mart=ensembl) %>%
+  filter((chromosome_name %in% as.character(1:23)))
 
-# df <- as.data.frame(nrommat[rownames(nrommat) == "JAK1",])
-# colnames(df) <- "values"
-# ggplot(df, aes(x = values)) + geom_histogram()
+train <- Hackathon2024.Training.Set.Peak2Gene.Pairs %>%
+  separate_wider_delim(col = peak, names = c("ch", "start", "end"), cols_remove = FALSE, delim = "-") %>%
+  left_join(martout, by = join_by("gene" == "hgnc_symbol")) %>%
+  mutate(chromosome_name = paste0("chr", chromosome_name),
+         start = as.numeric(start),
+         end = as.numeric(end),
+         start_position = as.numeric(start_position),
+         end_position = as.numeric(end_position),
+         dist = case_when(
+         ch == chromosome_name & pmax(start, end) < pmin(start_position, end_position) ~ pmin(start_position, end_position)-pmax(start, end),
+         ch == chromosome_name & pmin(start, end) > pmax(start_position, end_position) ~ pmin(start, end)-pmax(start_position, end_position),
+         ch == chromosome_name ~ 0,
+         TRUE ~ NA
+         )
+  )
+
+ggplot(train, aes(x = dist)) + geom_histogram() + facet_wrap(~Peak2Gene)
+
+
