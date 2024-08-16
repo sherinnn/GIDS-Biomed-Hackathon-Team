e89d6f4a95d6f4e94d4c0663783dc07387e69c9e
neanderthalensis <atyrlik@terpmail.umd.edu>
2024-08-16 00:42:21 -0400

    more stuff

--- preprocessing.R
+++ preprocessing.R
@@ -1,8 +1,8 @@
 library(Seurat)
 library(tidyverse)
 
-setwd("D:\\Users\\tyrli\\Documents\\Things\\R\\biosciencehackathon2024\\")
-
+# setwd("C:\\Users\\atyrlik\\Documents\\hackathon\\")
+setwd("D:\\Users\\tyrli\\Documents\\Things\\R\\biosciencehackathon2024")
 load("Hackathon2024.RData")
 
 
@@ -10,32 +10,36 @@ dat <- CreateSeuratObject(Hackathon2024.RNA, meta.data = Hackathon2024.Meta, ass
 dat[["ATAC"]] <- CreateAssay5Object(counts = Hackathon2024.ATAC)
 
 dat <- PercentageFeatureSet(dat, pattern = "^MT-", col.name = "mitofrac") #makes sure to start at the start of the string
-VlnPlot(dat, features = c("nCount_RNA", "nFeature_RNA", "mitofrac"))
-dat <- subset(dat, subset = nFeature_RNA > 500 & nFeature_RNA < 4000 & mitofrac < 20)
+VlnPlot(dat, features = c("nCount_ATAC", "nFeature_ATAC", "mitofrac"), group.by = "orig.ident")
+dat <- subset(dat, subset = nFeature_RNA > 500 & nFeature_RNA < 4000 & mitofrac < 20 & nFeature_ATAC > 500)
 
 dat <- dat %>%
   NormalizeData() %>%
   FindVariableFeatures() %>%
-  ScaleData(features = unique(c(VariableFeatures(dat), Hackathon2024.Training.Set.Peak2Gene.Pairs$gene, Hackathon2024.Testing.Set.Peak2Gene.Pairs$gene))) %>%
+  ScaleData() %>%
   RunPCA()
 
 ElbowPlot(dat)
 
 dat <- dat %>%
-  FindNeighbors(dims = 1:20) %>%
-  FindClusters(resolution = 0.3) %>%
-  RunUMAP(dims = 1:20)
+  FindNeighbors(dims = 1:50) %>%
+  FindClusters(resolution = 5, algorithm = 2) %>%
+  RunUMAP(dims = 1:50)
 
 dat <- dat %>%
   NormalizeData(assay = "ATAC") %>%
-  FindVariableFeatures(assay = "ATAC") %>%
-  ScaleData(assay = "ATAC", c(VariableFeatures(dat, assay = "ATAC"), Hackathon2024.Training.Set.Peak2Gene.Pairs$peak, Hackathon2024.Testing.Set.Peak2Gene.Pairs$peak)) %>%
-  RunPCA(assay = "ATAC", reduction.name = "pca_ATAC")
+  FindVariableFeatures(assay = "ATAC")# %>%
+#   ScaleData(assay = "ATAC") %>%
+#   RunPCA(assay = "ATAC")
+# 
+# ElbowPlot(dat, reduction = "pca_ATAC")
+# 
+# dat <- dat %>%
+#   FindNeighbors(dims = 1:50, reduction = "pca_ATAC") %>%
+#   FindClusters(., resolution = 0.2, graph.name = "ATAC_snn") %>%
+#   RunUMAP(dims = 1:50, reduction = "pca_ATAC", reduction.name = "umap_ATAC")
 
-ElbowPlot(dat, reduction = "pca_ATAC")
-
-dat <- dat %>%
-  FindNeighbors(dims = 1:14, reduction = "pca_ATAC") %>%
-  FindClusters(resolution = 0.3) %>%
-  RunUMAP(dims = 1:14, reduction = "pca_ATAC", reduction.name = "umap_ATAC")
-
+cRNA <- AggregateExpression(dat, assays = "RNA", group.by = "RNA_snn_res.4", normalization.method = "RC")$RNA
+colnames(cRNA) <- paste0("RNA_c", 1:ncol(cRNA))
+cATAC <- AggregateExpression(dat, assays = "ATAC", group.by = "RNA_snn_res.4", normalization.method = "RC")$ATAC
+colnames(cATAC) <- paste0("ATAC_c", 1:ncol(cATAC))
--- setgeneration.R
+++ setgeneration.R
@@ -1,6 +1,9 @@
 library(Seurat)
 library(tidyverse)
 library(biomaRt)
+library(GSVA)
+library(mclust)
+library(mltools)
 
 nrommat <- dat@assays$RNA$data
 atacnormmat <- dat@assays$ATAC$data
@@ -8,40 +11,32 @@ atacnormmat <- dat@assays$ATAC$data
 nrommat <- nrommat[rowSums(nrommat) > 0,]
 atacnormmat <- atacnormmat[rowSums(atacnormmat) > 0,]
 
+traintest <- Hackathon2024.Training.Set.Peak2Gene.Pairs
 
+#PC embeddings
+pcrna <- nrommat[rownames(nrommat) %in% c(VariableFeatures(dat), as.character(traintest$gene)), ] %>%
+  .[,colSums(.) > 0] %>%
+  prcomp(., scale. = TRUE, center = TRUE)
 
-pcrna <- prcomp(dat@assays$RNA$scale.data)
-pcatac <- prcomp(dat@assays$ATAC$scale.data)
+pcatac <- atacnormmat[rownames(atacnormmat) %in% c(VariableFeatures(dat, assay = "ATAC"), as.character(traintest$peak)), ] %>%
+  .[,colSums(.) > 0] %>%
+  prcomp(., scale. = TRUE, center = TRUE)
 
-
-pcatac$x[,1:100]
-
-train <- as.data.frame(pcrna$x[,1:100]) %>%
+train <- as.data.frame(pcrna$x[,1:356]) %>%
   rename_with(.fn = ~ paste0("RNA_", .x)) %>% 
   rownames_to_column() %>%
-  inner_join(Hackathon2024.Training.Set.Peak2Gene.Pairs, ., by = join_by("gene" == "rowname"))
+  left_join(traintest, ., by = join_by("gene" == "rowname"))
 
-train <- as.data.frame(pcatac$x[,1:1000]) %>%
+train <- as.data.frame(pcatac$x[,1:671]) %>%
   rename_with(.fn = ~ paste0("ATAC_", .x)) %>% 
   rownames_to_column() %>%
-  inner_join(train, ., by = join_by("peak" == "rowname"))
+  left_join(train, ., by = join_by("peak" == "rowname"))
+rm(pcrna, pcatac)
+#correlation
 
+# zmat <-(nrommat-rowMeans(nrommat))/apply(nrommat, 1, sd)
+ataczmat <-(atacnormmat-rowMeans(atacnormmat))/apply(atacnormmat, 1, sd)
 
-
-mean <- rowMeans(nrommat)
-sd <- apply(nrommat, 1, sd)                
-zmat <-(nrommat-mean)/sd
-
-mean <- rowMeans(atacnormmat)
-sd <- apply(atacnormmat, 1, sd)               
-ataczmat <-(atacnormmat-mean)/sd
-
-genesets <- split(zmat, rownames(zmat))
-genesets <- lapply(genesets, function(x){ 
-  names(x) <-colnames(zmat)
-  x[x > 0]
-})
-names(genesets) <- rownames(nrommat)
 
 atacsets <- split(ataczmat, rownames(ataczmat))
 atacsets <- lapply(atacsets, function(x){ 
@@ -50,41 +45,34 @@ atacsets <- lapply(atacsets, function(x){
 })
 names(atacsets) <- rownames(ataczmat)
 
-subgenesets <- genesets[names(genesets) %in% Hackathon2024.Training.Set.Peak2Gene.Pairs$gene]
-subatacsets <- atacsets[names(atacsets) %in% Hackathon2024.Training.Set.Peak2Gene.Pairs$peak]
+# subgenesets <- genesets[names(genesets) %in% traintest$gene]
+subatacsets <- atacsets[names(atacsets) %in% traintest$peak]
 
+#gsva
 
-temp <- lapply(subgenesets, function(x){
-  out <- lapply(subatacsets, function(y){
-    rnadf <- as.data.frame(x) %>%
-      rownames_to_column()
-    colnames(rnadf) <- c("rname","RNA")
-    atacdf <- as.data.frame(y) %>%
-      rownames_to_column()
-    colnames(atacdf) <- c("rname","ATAC")
-    df <- full_join(rnadf, atacdf, by = join_by("rname"))%>%
-      mutate(RNA = replace_na(RNA, 0),
-             ATAC = replace_na(ATAC, 0))
-    summary(lm(RNA~ATAC, data=df))$r.squared
-  })
+namedsubatacsets <- lapply(subatacsets, names)
+
+param <- gsvaParam(t(nrommat[rownames(nrommat) %in% traintest$gene,]), namedsubatacsets, minSize = 1)
+gsvaout <- gsva(param, verbose=TRUE)
+
+gsvascores <- apply(train, 1, function(x){
+  if(x[["peak"]] %in% rownames(gsvaout) & x[["gene"]] %in% colnames(gsvaout)){gsvaout[x[["peak"]], x[["gene"]]]}
+  else{NA}
 })
-summary(lm(RNA~ATAC, data=df))$r.squared
-tempmat <- do.call(rbind, temp)
 
-scores <- apply(train, 1, function(x){
-  tempmat[as.character(x[["gene"]]),x[["peak"]]]
-})
-train$corr <- unlist(scores)
-
+train$gsva <- gsvascores
+rm(ataczmat, param, gsvaout)
+#physical distance
 ensembl <- useMart("ensembl")
 ensembl <- useDataset("hsapiens_gene_ensembl",mart=ensembl)
 martout <- getBM(attributes=c('hgnc_symbol', 'chromosome_name', 'start_position', 'end_position'),
       filters= 'hgnc_symbol',
-      values= toupper(Hackathon2024.Training.Set.Peak2Gene.Pairs$gene),
+      values= toupper(traintest$gene),
       mart=ensembl) %>%
-  filter((chromosome_name %in% as.character(1:23)))
+  filter(chromosome_name %in% as.character(1:23)) %>%
+  distinct(hgnc_symbol, .keep_all = TRUE)
 
-train <- Hackathon2024.Training.Set.Peak2Gene.Pairs %>%
+out <- traintest %>%
   separate_wider_delim(col = peak, names = c("ch", "start", "end"), cols_remove = FALSE, delim = "-") %>%
   left_join(martout, by = join_by("gene" == "hgnc_symbol")) %>%
   mutate(chromosome_name = paste0("chr", chromosome_name),
@@ -100,6 +88,7 @@ train <- Hackathon2024.Training.Set.Peak2Gene.Pairs %>%
          ), 
          overlap = case_when(
           dist == 0 & between(start, pmin(start_position, end_position), pmax(start_position, end_position)) & between(start, pmin(start_position, end_position), pmax(start_position, end_position)) ~ abs(start-end),
+          dist == 0 & between(start_position, pmin(start, end), pmax(start, end)) & between(end_position, pmin(start, end), pmax(start, end)) ~ abs(start-end),
           dist == 0 & between(pmin(start, end), pmin(start_position, end_position), pmax(start_position, end_position)) ~ pmax(start_position, end_position) -pmin(start, end),
           dist == 0 & between(pmax(start, end), pmin(start_position, end_position), pmax(start_position, end_position)) ~ pmax(start, end) - pmin(start_position, end_position),
           dist > 0 ~ 0,
@@ -107,10 +96,54 @@ train <- Hackathon2024.Training.Set.Peak2Gene.Pairs %>%
          )
   )
 
-prcomp(nrommat, scale = TRUE)
+train$dist <- out$dist
+train$overlap <- out$overlap
+train <- mutate(train, overlap = replace_na(overlap, 0))
+
+train <- train %>%
+  left_join(as.data.frame(cRNA) %>% rownames_to_column(), by = join_by("gene" == "rowname")) %>%
+  left_join(as.data.frame(cATAC) %>% rownames_to_column(), by = join_by("peak" == "rowname"))
+
+mattrain <- train %>%
+  filter(as.logical(Peak2Gene)) %>%
+  sample_n(size = 20) %>% 
+  bind_rows(
+    train %>%
+      filter(!as.logical(Peak2Gene)) %>%
+      sample_n(size = 20)
+  ) %>%
+  dplyr::select(matches("_c"))
 
 
-ggplot(train, aes(x = corr)) + geom_histogram() + facet_wrap(~Peak2Gene)
+# these <- train %>% 
+#   filter(is.na(dist)) %>% 
+#   magrittr::extract2("gene")
 
-write_csv(train, "D:\\Users\\tyrli\\Documents\\Hackathon2024.Training.Set.Peak2Gene.Pairs.pcloadings.csv")
+trainmodel <- MclustDA(as.matrix(mattrain), c(rep(TRUE, 20), rep(FALSE, 20)))
+cout <- predict(trainmodel, newdata = as.matrix(train %>% dplyr::select(matches("overlap"))))
 
+# check <- Hackathon2024.Training.Set.Peak2Gene.Pairs %>%
+#   filter(!(gene %in% these))
+check <- Hackathon2024.Training.Set.Peak2Gene.Pairs
+check$classification <- out$classification
+check$classification_clust <- cout$classification
+
+check$gsva <- train$gsva
+check$scoreoverlap <-out$z[,"TRUE"]
+check$scoreclust <-cout$z[,"TRUE"]
+
+check %>%
+  group_by(Peak2Gene, classification, classification_clust) %>%
+  summarise(n = n(), gsva = mean(gsva))
+
+
+mcc(as.logical(check$Peak2Gene), as.logical(check$consensus))
+
+
+
+
+
+ggplot(train, aes(x = gsva)) + geom_histogram() + facet_wrap(~Peak2Gene)
+
+write_csv(train, "Hackathon2024.Training.Set.Peak2Gene.Pairs.everything.csv")
+train <- read_csv("Hackathon2024.Training.Set.Peak2Gene.Pairs.everything.csv")
